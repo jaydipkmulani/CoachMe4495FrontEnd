5d9e472b7682964fe0b6fbf90dfd9d569d87456d
jaydipkmulani <jaydipkmulani@gmail.com>
2023-08-08 17:10:51 -0700

    navcgvgd

--- src/components/vidocall/call.jsx
+++ src/components/vidocall/call.jsx
@@ -1,49 +1,44 @@
 import * as React from 'react';
 import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
-
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 export default function App() {
-    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
     const { roomID } = useParams();
 
     let myMeeting = async (element) => {
-        // generate Kit Token
         const appID = 497473618;
-        const serverSecret = "9964f61987242f449f86cfa5811c842d";
-        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, currentUser._id, currentUser.username);
+        const serverSecret = '9964f61987242f449f86cfa5811c842d';
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+            appID,
+            serverSecret,
+            roomID,
+            currentUser._id,
+            currentUser.username
+        );
 
-
-        // Create instance object from Kit Token.
         const zp = ZegoUIKitPrebuilt.create(kitToken);
-// Add event listener to handle call disconnect
-    // start the call
+
+        // Join the room
         zp.joinRoom({
             container: element,
             sharedLinks: [
                 {
                     name: 'Personal link',
-                    url:
-                        window.location.protocol + '//' +
-                        window.location.host + window.location.pathname +
-                        '?roomID=' +
-                        roomID,
+                    url: window.location.href,
                 },
             ],
             scenario: {
-                mode: ZegoUIKitPrebuilt.GroupCall, 
+                mode: ZegoUIKitPrebuilt.GroupCall,
             },
         });
-
-
     };
 
     return (
-        <div
-            ref={myMeeting}
-            style={{ width: '100vw', height: '100vh' }}
-        >
-           <Link to="/"><button className='button'>back to home </button></Link> 
+        <div ref={myMeeting} style={{ width: '100vw', height: '100vh' }}>
+            <Link to="/">
+                <button className="button">Back to Home</button>
+            </Link>
         </div>
     );
 }
