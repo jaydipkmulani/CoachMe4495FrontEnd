7e41b97ebe8d5e671c2c18fa50c6400724c89ddc
jaydipkmulani <jaydipkmulani@gmail.com>
2023-08-08 17:44:20 -0700

    navcgfvggd

--- src/components/vidocall/call.jsx
+++ src/components/vidocall/call.jsx
@@ -1,48 +1,41 @@
 import * as React from 'react';
 import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
-
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 export default function App() {
-    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
     const { roomID } = useParams();
 
     let myMeeting = async (element) => {
-        // generate Kit Token
-        const appID = 497473618;
-        const serverSecret = "9964f61987242f449f86cfa5811c842d";
-        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, currentUser._id, currentUser.username);
+        const appID = 1774145697;
+        const serverSecret = '986c8d720b8c8a8e4e4f46eb0bec87f5';
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+            appID,
+            serverSecret,
+            roomID,
+            currentUser._id,
+            currentUser.username
+        );
 
-
-        // Create instance object from Kit Token.
         const zp = ZegoUIKitPrebuilt.create(kitToken);
-// Add event listener to handle call disconnect
-    // start the call
+
+        // Join the room with one-on-one tutoring scenario
         zp.joinRoom({
             container: element,
             sharedLinks: [
                 {
                     name: 'Personal link',
-                    url:
-                        window.location.protocol + '//' +
-                        window.location.host + window.location.pathname +
-                        '?roomID=' +
-                        roomID,
+                    url: window.location.href,
                 },
             ],
             scenario: {
-                mode: ZegoUIKitPrebuilt.GroupCall, 
+                mode: ZegoUIKitPrebuilt.OneToOne,
             },
         });
-
-
     };
 
     return (
-        <div
-            ref={myMeeting}
-            style={{ width: '100vw', height: '100vh' }}
-        >
+        <div ref={myMeeting} style={{ width: '100vw', height: '100vh' }}>
            
         </div>
     );
