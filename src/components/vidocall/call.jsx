ecbf7e3e126393d9f76354e62c414e8af8b5dda8
jaydipkmulani <jaydipkmulani@gmail.com>
2023-08-08 19:13:53 -0700

    navcgfvgfvfgffd

--- src/components/vidocall/call.jsx
+++ src/components/vidocall/call.jsx
@@ -1,19 +1,19 @@
 import * as React from 'react';
 import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 export default function App() {
     const currentUser = JSON.parse(localStorage.getItem('currentUser'));
     const { roomID } = useParams();
 
     let myMeeting = async (element) => {
-        const appID = 1774145697;
-        const serverSecret = '986c8d720b8c8a8e4e4f46eb0bec87f5';
+        const appID = 893576107;
+        const serverSecret = '980391f1b57ac5e5728db5747167f87b';
         const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
             appID,
             serverSecret,
             roomID,
-            currentUser._id,
+            Date.now().toString(),
             currentUser.username
         );
 
@@ -22,12 +22,7 @@ export default function App() {
         // Join the room with one-on-one tutoring scenario
         zp.joinRoom({
             container: element,
-            sharedLinks: [
-                {
-                    name: 'Personal link',
-                    url: window.location.href,
-                },
-            ],
+          
             scenario: {
                 mode: ZegoUIKitPrebuilt.GroupCall,
             },
